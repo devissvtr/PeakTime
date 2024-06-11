3f7d9f995a2064cb1777a557d1c38c57228c694a
Abdullah-Rizky-Adam-Safero <abdrizkyfero@gmail.com>
2024-06-11 07:31:08 +0700

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,9 @@ dependencies {
 
     implementation("com.github.AnyChart:AnyChart-Android:1.1.2")
     implementation("androidx.multidex:multidex:2.0.1")
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 }
